2a5ca1b04409b7a14a090b85518cfd86e1bdec45
oliver <oliver.margetts@gmail.com>
2026-04-21 16:31:53 +0100

    🙃 add deadlocking 'if threadpool full' code to demo race condition

--- fastapi/concurrency.py
+++ fastapi/concurrency.py
@@ -18,10 +18,9 @@ _T = TypeVar("_T")
 # Blocking __exit__ and other teardown operations from running can create race
 # conditions/deadlocks if the context manager itself has its own internal pool
 # (e.g. a database connection pool).
-# To avoid this maintain a separate limiter for teardown operations, so that the
-# operations acquiring resources can never block operations releasing resources.
-# NOTE: 5 is arbitrary, we would like more than 1 so that teardowns are not serialised.
-_teardown_limiter = CapacityLimiter(5)
+# To avoid this maintain a separate limiter for teardown operations, and attempt
+# to use it if the main threadpool is full.
+_deadlock_overflow_limiter = CapacityLimiter(1)
 
 
 @asynccontextmanager
@@ -32,24 +31,30 @@ async def contextmanager_in_threadpool(
         yield await run_in_threadpool(cm.__enter__)
     except Exception as e:
         ok = bool(
-            await run_in_teardown_threadpool(cm.__exit__, type(e), e, e.__traceback__)
+            await run_in_threadpool_with_overflow(
+                cm.__exit__, type(e), e, e.__traceback__
+            )
         )
         if not ok:
             raise e
     else:
-        await run_in_teardown_threadpool(cm.__exit__, None, None, None)
+        await run_in_threadpool_with_overflow(cm.__exit__, None, None, None)
 
 
-async def run_in_teardown_threadpool(
+async def run_in_threadpool_with_overflow(
     func: Callable[_P, _T], *args: _P.args, **kwargs: _P.kwargs
 ) -> _T:
-    """Run a function in the separate teardown threadpool.
+    """Run a function in the threadpool, or if is full use the overflow pool.
 
-    This will run the function in the teardown threadpool in order to avoid it
-    being blocked by other operations waiting to acquire resources.
+    This will run the function a separate threadpool if the main one is full in order
+    to avoid being blocked by other operations waiting to acquire resources.
 
     Unless you know what you are doing, you probably don't want this function,
     use run_in_threadpool instead.
     """
     func = functools.partial(func, *args, **kwargs)
-    return await anyio.to_thread.run_sync(func, limiter=_teardown_limiter)
+
+    if anyio.to_thread.current_default_thread_limiter().available_tokens > 0:
+        return await run_in_threadpool(func)
+
+    return await anyio.to_thread.run_sync(func, limiter=_deadlock_overflow_limiter)
